efa61f3c84f9e8bd03577eb71d83cf7e6ad1b9d5
Tetiana Dub <112940506+Tasha852209@users.noreply.github.com>
2023-08-04 17:32:46 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = '';
 
+startBtn.addEventListener('click', onStartClick);
+
+function onStartClick() {
+  startBtn.disabled = true;
+  timerId = setInterval(
+    () => (body.style.backgroundColor = getRandomHexColor()),
+    1000
+  );
+}
+
+stopBtn.addEventListener('click', onStopClick);
+
+function onStopClick() {
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
